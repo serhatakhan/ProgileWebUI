97b113ede1d33d293988bd553e8e6a903e7a7064
SaruGit75 <srhnfrkn75@gmail.com>
2024-04-07 15:35:28 +0300

    logo added and positioned

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -4,111 +4,119 @@ import { IoLogoGithub } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa";
 
 const AuthPage = () => {
-  // tıklama olayının state'ini tut
-  const [isSignUp, setIsSignUp] = useState(true);
+    // tıklama olayının state'ini tut
+    const [isSignUp, setIsSignUp] = useState(true);
 
-  const handleSignUpClick = () => {
-    setIsSignUp(true);
-  };
+    const handleSignUpClick = () => {
+        setIsSignUp(true);
+    };
 
-  const handleSignInClick = () => {
-    setIsSignUp(false);
-  };
+    const handleSignInClick = () => {
+        setIsSignUp(false);
+    };
 
-  return (
-    <div className="content">
-      <div className="header">
-        <div className="logo_section">
-          <img src="/public/logo.jpg" alt="logo" />
-        </div>
-      </div>
+    return (
+        <div className="content">
+            {/* <div className="header">
+                <img src="../progile_logo3.png" alt="logo" />
+            </div> */}
 
-      <div className="auth-sec">
-        <div
-          className={`container ${isSignUp ? "right-panel-active" : ""}`}
-          id="main"
-        >
-          {/* <img src="/postit2.png" height={100} className="postit" /> */}
-          <div className="sign-up">
-            <form action="#">
-              <h1>Create Account</h1>
-              <div className="social-container">
-                <a href="#" className="social">
-                  <FaGoogle />
-                </a>
-                <a href="#" className="social">
-                  <IoLogoGithub />
-                </a>
-                <a href="#" className="social">
-                  <FaLinkedin />
-                </a>
-              </div>
-              <p>or use your email for registration</p>
-              <input type="text" name="txt" placeholder="Name" required="" />
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                required=""
-              />
-              <input
-                type="password"
-                name="pswd"
-                placeholder="Password"
-                required=""
-              />
-              <button>Register</button>
-            </form>
-          </div>
-          <div className="sign-in">
-            <form action="#">
-              <h1>Sign in</h1>
-              <div className="social-container">
-                <a href="#" className="social">
-                  <FaGoogle />
-                </a>
-                <a href="#" className="social">
-                  <IoLogoGithub />
-                </a>
-                <a href="#" className="social">
-                  <FaLinkedin />
-                </a>
-              </div>
-              <p>or use your account</p>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                required=""
-              />
-              <input
-                type="password"
-                name="pswd"
-                placeholder="Password"
-                required=""
-              />
-              <a href="#" style={{color: "#fff"}}>Forget your Password?</a>
-              <button>Sign In</button>
-            </form>
-          </div>
-          <div className="overlay-container">
-            <div className="overlay">
-              <div className="overlay-left">
-                <button id="signIn" onClick={handleSignInClick}>
-                  Sign In
-                </button>
-              </div>
-              <div className="overlay-right">
-                <button id="signUp" onClick={handleSignUpClick}>
-                  Register
-                </button>
-              </div>
+            <div className="auth-sec">
+                <div
+                    className={`container ${
+                        isSignUp ? "right-panel-active" : ""
+                    }`}
+                    id="main"
+                >
+                    <img src="../progile_logo3.png" className="login_logo" alt="logo" />
+
+                    <div className="sign-up">
+                        <form action="#">
+                            <h1>Create Account</h1>
+                            <div className="social-container">
+                                <a href="#" className="social">
+                                    <FaGoogle />
+                                </a>
+                                <a href="#" className="social">
+                                    <IoLogoGithub />
+                                </a>
+                                <a href="#" className="social">
+                                    <FaLinkedin />
+                                </a>
+                            </div>
+                            <p>or use your email for registration</p>
+                            <input
+                                type="text"
+                                name="txt"
+                                placeholder="Name"
+                                required=""
+                            />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                required=""
+                            />
+                            <input
+                                type="password"
+                                name="pswd"
+                                placeholder="Password"
+                                required=""
+                            />
+                            <button>Register</button>
+                        </form>
+                    </div>
+                    <div className="sign-in">
+                        <form action="#">
+                            <h1>Sign in</h1>
+                            <div className="social-container">
+                                <a href="#" className="social">
+                                    <FaGoogle />
+                                </a>
+                                <a href="#" className="social">
+                                    <IoLogoGithub />
+                                </a>
+                                <a href="#" className="social">
+                                    <FaLinkedin />
+                                </a>
+                            </div>
+                            <p>or use your account</p>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                required=""
+                            />
+                            <input
+                                type="password"
+                                name="pswd"
+                                placeholder="Password"
+                                required=""
+                            />
+                            <a href="#" style={{ color: "#fff" }}>
+                                Forget your Password?
+                            </a>
+                            <button>Sign In</button>
+                        </form>
+                    </div>
+                    <div className="overlay-container">
+                        <div className="overlay">
+                            <div className="overlay-left">
+                                <button id="signIn" onClick={handleSignInClick}>
+                                    Sign In
+                                </button>
+                            </div>
+                            <div className="overlay-right">
+                                <button id="signUp" onClick={handleSignUpClick}>
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AuthPage;
