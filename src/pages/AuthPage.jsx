cb426c90fbfef96cbe28a723da4464bff09337ef
SaruGit75 <srhnfrkn75@gmail.com>
2024-04-20 23:56:32 +0300

    axios ile ilk request basarılı sekilde atıldı.

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -3,6 +3,7 @@ import { FaGoogle } from "react-icons/fa";
 import { IoLogoGithub } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa";
 import styles from "../styles/auth/auth.module.css";
+import axios from "axios";
 
 const AuthPage = () => {
     // tıklama olayının state'ini tut
@@ -15,38 +16,44 @@ const AuthPage = () => {
         setIsSignUp(false);
     };
 
-
     // hesap oluşturulduğunda formun gönderilme olayını izler
     const registerSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        console.log(e);
 
-        const formData = new FormData(e.target)
-        const registerData = Object.fromEntries(formData.entries())
+        const formData = new FormData(e.target);
+        const registerData = Object.fromEntries(formData.entries());
 
-        console.log(registerData)
+        console.log(registerData);
 
         // api'ye veriyi yolla-post
+        axios.post('https://localhost:7212/api/Users/CreateUser',     registerData)
+            .then((resp) => {
+                console.log(resp);
+                if(resp.data.responseStatus == 2)
+                {
+                    alert(resp.data.errors[0])
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
 
         // hesap oluşturma başarılı ise kullanıcıya bildir
-
-    }
+    };
 
     // giriş yapılırken formun gönderilme olayını izler
     const loginSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        const formData = new FormData(e.target)
-        const loginData = Object.fromEntries(formData.entries())
+        const formData = new FormData(e.target);
+        const loginData = Object.fromEntries(formData.entries());
 
-        console.log(loginData)
+        console.log(loginData);
 
-        // 
+        //
 
-        // 
-    }
-
-
-
+        //
+    };
 
     return (
         <div className={styles.content}>
@@ -78,24 +85,50 @@ const AuthPage = () => {
                                 </a>
                             </div>
                             <p>or use your email for registration</p>
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Name"
-                                required
-                            />
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Email"
-                                required=""
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                                required=""
-                            />
+
+                            <div className={styles.inputs}>
+                                <div className={styles.first_column}>
+                                    <input
+                                        type="text"
+                                        name="firstName"
+                                        placeholder="Firstname"
+                                        required
+                                    />
+                                    <input
+                                        type="text"
+                                        name="lastName"
+                                        placeholder="Lastname"
+                                        required
+                                    />
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        required=""
+                                    />
+                                </div>
+                                <div className={styles.second_column}>
+                                    <input
+                                        type="text"
+                                        name="userName"
+                                        placeholder="Username"
+                                        required
+                                    />
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        required=""
+                                    />
+                                    <input
+                                        type="password"
+                                        name="passwordConfirm"
+                                        placeholder="Password Confirm"
+                                        required=""
+                                    />
+                                </div>
+                            </div>
+
                             <button type="submit">Register</button>
                         </form>
                     </div>
@@ -114,27 +147,32 @@ const AuthPage = () => {
                                 </a>
                             </div>
                             <p>or use your account</p>
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Email"
-                                required
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                                required
-                            />
+                            <div className={styles.signin_inputs}>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email"
+                                    required
+                                />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    required
+                                />
+                            </div>
+
                             <a href="#" style={{ color: "#fff" }}>
                                 Forget your Password?
                             </a>
-                            {/* <button>Sign In</button> */}
 
                             <span className={styles.btn}>
-                              <img src="../../public/postit2.png" className={styles.postit}/>
-                              <span className={styles.front}>Sign In 🚀</span>
-                              <span className={styles.back}></span>
+                                <img
+                                    src="../../public/postit_4.png"
+                                    className={styles.postit}
+                                />
+                                <span className={styles.front}>Sign In</span>
+                                <span className={styles.back}></span>
                             </span>
                         </form>
                     </div>
